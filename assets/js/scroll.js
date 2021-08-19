95638e51228b9722143524f7e54b1de682c1622c
alswjd1125 <alslal1125@naver.com>
2021-08-19 10:58:19 +0900

    js scroll
    js scroll

---
+++ assets/js/scroll.js
@@ -0,0 +1,4 @@
+const bg = document.querySelector('.img-scroll');
+            window.addEventListener('scroll',function(){
+            bg.style.width =50 +window.pageYOffset/20+'%';
+            });
