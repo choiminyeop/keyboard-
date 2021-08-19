2fa34ba39123c2f03eefdbf39cbc6727c3fda131
alswjd1125 <alslal1125@naver.com>
2021-08-19 12:42:03 +0900

    a
    a

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,3 +2,10 @@ const bg = document.querySelector('.img-scroll');
             window.addEventListener('scroll',function(){
             bg.style.width =50 +window.pageYOffset/30+'%';
             });
+
+
+const scrollBg = document.querySelector('.slider-area');
+window.addEventListener('scroll', function(){
+    scrollBg.style.backgroundSize = 100 - +window.pageYOffset/30+ '%';
+    scrollBg.style.opacity = 1.5 - +window.pageYOffset/700+'';
+})
